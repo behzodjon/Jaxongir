87a56614b22d741328c9dfe1db1e08fb700c3e25
jaxongir <Ismoilovj2018@gmail.com>
2018-10-09 23:15:20 +0500

    First commit

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::get('/', function () {
+    return view('index');
+});
+Route::group(['middleware' => 'web'], function(){
+    Route::match(['get', 'post'], '/', ['uses' => 'IndexController@execute', 'as' => 'home']);
+    Route::get ('/about', ['uses' => 'AboutController@show']);
+    Route::get ('/servises', ['uses' => 'ServisesController@show']);
+    Route::get ('/news', ['uses' => 'NewsController@show']);
+    Route::get('/contact', ['uses' => 'ContactController@show']);
+});
